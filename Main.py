8dcfaa14cccd77fb936e85b1b4eba9db32670886
URK21CS3032 <110385645+URK21CS3032@users.noreply.github.com>
2022-09-12 16:10:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,41 +4,50 @@ class Node:
     self.next = None
 
 
-class Stack:
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
-   new_node=Node(data)
-   new_node.next=self.head
-   self.head=new_node
+  def enqueue(self, data) -> None:
+    # Write your code here
+    temp = Node(data)
+    if self.head == None:
+      self.head = temp
+      self.last = temp
+      temp.next = None
+    else:
+      self.last.next = temp
+      temp.next = None
+      self.last = temp
 
-  def pop(self) -> None:
-    if self.head != None:
-      self.head=self.head.next
-  
-  def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    elements = ''
-    current = self.head
-    while current != None:
-      elements += str(current.data)+"=>"
-      current = current.next
-    print(elements+"None")
+  def dequeue(self) -> None:
+    # Write your code here
+    if self.head == None:
+      pass
+    else:
+      self.head = self.head.next
 
-    
+  def status(self) -> None:
+    # Write your code here
+    temp = self.head
+    while temp!= None:
+      print(temp.data, "=>", sep="", end = "")
+      temp = temp.next
+    print("None")
+
+
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
+
